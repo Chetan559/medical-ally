77bc2a7b811d389a9be5b58111b452ddc1b5c1bd
Chetan Sharma <cschetan559@gmail.com>
2024-12-20 21:08:42 +0530

    Information section added

--- apps/Medical-Ally/src/Pages/User/Home/Home.jsx
+++ apps/Medical-Ally/src/Pages/User/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../../Components/Header/Header";
 import Footer from "../../../Components/Footer/Footer";
 import Hero from "../../../Components/hero/Hero";
+import Info from "../../../Components/User-Info/Info";
 
 function Home() {
   return (
     <div className="home-section">
       <Header />
       <Hero />
+      <Info />
       <Footer />
     </div>
   );
