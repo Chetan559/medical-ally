9d4fae7a9919378d757c0880ab88502696d8e040
Chetan Sharma <cschetan559@gmail.com>
2024-12-20 21:17:36 +0530

    Admin page initiated

--- apps/Medical-Ally/src/Pages/Admin/Admin.jsx
+++ apps/Medical-Ally/src/Pages/Admin/Admin.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Admin() {
-  return <div></div>;
+  return (
+    <div>
+      <h1> This is admin page</h1>
+      <h1> here admin will monitor</h1>
+    </div>
+  );
 }
 
 export default Admin;
