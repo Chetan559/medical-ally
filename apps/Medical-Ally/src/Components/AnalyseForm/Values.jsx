2ec06ddce4c1b48d1ff0f19b93ba6082a9a50a43
Chetan Sharma <cschetan559@gmail.com>
2024-12-22 03:16:25 +0530

    step 1 in analyse form created

---
+++ apps/Medical-Ally/src/Components/AnalyseForm/Values.jsx
@@ -0,0 +1,53 @@
+export const genders = [
+  {
+    value: "Male",
+    label: "M",
+  },
+  {
+    value: "Female",
+    label: "F",
+  },
+  {
+    value: "Transgender",
+    label: "T",
+  },
+  {
+    value: "Other",
+    label: "Other",
+  },
+];
+
+export const blood = [
+  {
+    value: "A Positive",
+    label: "A+",
+  },
+  {
+    value: "A Negetive",
+    label: "A-",
+  },
+  {
+    value: "O Positive",
+    label: "O+",
+  },
+  {
+    value: "O Negetive",
+    label: "O-",
+  },
+  {
+    value: "AB Positive",
+    label: "AB+",
+  },
+  {
+    value: "AB Negetive",
+    label: "AB-",
+  },
+  {
+    value: "B Positive",
+    label: "B+",
+  },
+  {
+    value: "B Negetive",
+    label: "B-",
+  },
+];
